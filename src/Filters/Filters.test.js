0d1b1ee669e0319380a989bf35be8211f352a909
Ethan Freestone <ethan.freestone@k-int.co.uk>
2022-11-10 18:20:29 +0000

    test: stripes-erm-testing
    Added jest manual mocks and utilised those in tests

--- src/Filters/Filters.test.js
+++ src/Filters/Filters.test.js
@@ -1,8 +1,8 @@
 import { React } from 'react';
 import { waitFor } from '@testing-library/dom';
 
-import '@folio/stripes-erm-components/test/jest/__mock__';
-import { renderWithIntl } from '@folio/stripes-erm-components/test/jest/helpers';
+
+import { renderWithIntl } from '@folio/stripes-erm-testing';
 import { Accordion, Checkbox, Selection, SelectionList as SelectListInteractor } from '@folio/stripes-testing';
 import { MemoryRouter } from 'react-router-dom';
 
